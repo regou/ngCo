47933ab17c93a305e9ddeb0138be043b5e403d1f
wangxiao <wangxiao@patsnap.com>
2016-09-06 18:48:49 +0800

    deal with uglify

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 'use strict';
-var isGeneratorFn = require('is-generator').fn
 
 angular.module('ng-co', [])
 	.factory('$co', require('./co_$q.js'));
 
 function coc (serviceFunction) {
-	if(isGeneratorFn(serviceFunction)){
-		return function () {
-			var co = arguments[0];
-			co(serviceFunction.apply(this,arguments))
-				.catch(function (error) {
-					console.error(error);
-				});
-		}
-	}else{
-		return serviceFunction;
+	return function () {
+		var co = arguments[0];
+		co(serviceFunction.apply(this,arguments))
+			.catch(function (error) {
+				console.error(error);
+			});
 	}
 }
 
